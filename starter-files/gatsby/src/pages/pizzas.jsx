8aca08d48d2bdc39d59261e6aa8d8076bc3a1951
Joseph Geringer <joegeringer@gmail.com>
2021-05-21 18:57:36 -0500

    master-gatsby module 20

--- starter-files/gatsby/src/pages/pizzas.jsx
+++ starter-files/gatsby/src/pages/pizzas.jsx
@@ -1,9 +1,38 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+    const pizzas = data.pizzas.nodes;
+    console.log(data.pizzas);
     return (
     <>
-        <p>Hey! I'm the Pizzas page!</p>
+        <PizzaList pizzas={pizzas} />
     </>
     );
 }
+
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes {
+                id
+                name
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+                image {
+                    asset {
+                        fluid(maxWidth: 400) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
