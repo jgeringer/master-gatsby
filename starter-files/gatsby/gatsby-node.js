972598720ac92779b95ec56da4123323724005d3
Joseph Geringer <joegeringer@gmail.com>
2021-07-10 11:18:33 -0500

    master-gatsby module 27

--- starter-files/gatsby/gatsby-node.js
+++ starter-files/gatsby/gatsby-node.js
@@ -1,65 +1,102 @@
 import path from 'path';
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
-    // 1. Get a template for this page
-    const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
-    // 2. Query all pizzas
-    const { data } = await graphql(`
-        query {
-            pizzas: allSanityPizza {
-                nodes {
-                    name
-                    slug {
-                        current
-                    }
-                }
-            }
+  // 1. Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
         }
-    `);
-    // 3. Loop over each pizza and create a page for that pizza
-    data.pizzas.nodes.forEach(pizza => {
-        console.log(`creating a page for: ${pizza.name}`)
-        actions.createPage({
-            path: `pizza/${pizza.slug.current}`,
-            component: pizzaTemplate,
-            context: {
-                slug: pizza.slug.current,
-            }
-        });
-    })
+      }
+    }
+  `);
+  // 3. Loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    // console.log(`creating a page for: ${pizza.name}`);
+    actions.createPage({
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
 }
 
 async function turnToppingsIntoPages({ graphql, actions }) {
-    const toppingsTemplate = path.resolve('./src/pages/pizzas.js');
+  const toppingsTemplate = path.resolve('./src/pages/pizzas.js');
 
-    const { data } = await graphql(`
-        query {
-            toppings: allSanityToppings {
-                nodes {
-                    name
-                    id
-                }
-            }
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityToppings {
+        nodes {
+          name
+          id
         }
-    `);
+      }
+    }
+  `);
 
-    data.toppings.nodes.forEach((topping) => {
-        console.log(`Creating page for topping`, topping.name);
-        actions.createPage({
-            path: `topping/${topping.name}`,
-            component: toppingsTemplate,
-            context: {
-                topping: topping.name,
-            }
-        });
-    })
+  data.toppings.nodes.forEach((topping) => {
+    console.log(`Creating page for topping`, topping.name);
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingsTemplate,
+      context: {
+        topping: topping.name,
+      },
+    });
+  });
+}
+
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  console.log('Turn Beer into Nodes!');
+  // 1. Fetch a list of beers
+  const res = await fetch('https://api.sampleapis.com/beers/ale');
+  const beers = await res.json();
+  console.log(beers);
+  // 2. Loop over each one
+  for (const beer of beers) {
+    // create a node for each beer
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    // 3. Create a node for that beer
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+}
+
+export async function sourceNodes(params) {
+  // fetch a list of external beers and source them into our gatsby API!
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
 }
 
 export async function createPages(params) {
-    // Create pages dynamically...
-    // Wait for all promizes to be resolved befor finishing this function
-    await Promise.all([
-        turnPizzasIntoPages(params),
-        turnToppingsIntoPages(params),
-    ]);
+  // Create pages dynamically...
+  // Wait for all promizes to be resolved befor finishing this function
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
 }
